3d5a1c5e12938b16d51f596ea1619941d076a73c
Ivan riboldi <38292854@comunidadort.edu.ar>
2026-06-07 23:14:57 -0300

    new button common

---
+++ src/components/ButtonCommon.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    @click="!disabled && $emit('click')"
+    :disabled="disabled"
+    class="py-2.5 px-4 font-bold rounded-xl text-sm transition duration-300"
+    :class="
+      disabled
+        ? 'bg-gray-100 text-gray-400 cursor-not-allowed'
+        : 'bg-indigo-50 text-indigo-700 hover:bg-indigo-600 hover:text-white cursor-pointer'
+    "
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+defineEmits(["click"]);
+</script>
