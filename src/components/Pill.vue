39ba462394b238c48da9498882793a915c53f3f2
Ivan riboldi <38292854@comunidadort.edu.ar>
2026-06-07 23:13:56 -0300

    new component pill

---
+++ src/components/Pill.vue
@@ -0,0 +1,11 @@
+<template>
+  <span  class="px-2 py-1 rounded-full bg-white text-indigo-700 text-xs font-semibold">
+    $ {{ props.tarifa }}/hora
+  </span>
+</template>
+
+<script setup>
+const props = defineProps({
+  tarifa: Number,
+});
+</script>
