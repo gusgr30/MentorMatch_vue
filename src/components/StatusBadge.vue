80a1fb713be1f71a6ad4f6a541ad9877d624246d
Ivan riboldi <38292854@comunidadort.edu.ar>
2026-06-10 22:42:52 -0300

    se crea Badge

---
+++ src/components/StatusBadge.vue
@@ -0,0 +1,23 @@
+<template>
+  <span class="badge badge-outline font-bold uppercase text-xs min-w-30 justify-center" :class="badgeClass">
+    {{ estado }}
+  </span>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { ESTADOS_RESERVA } from '../constants/index.js'
+
+const props = defineProps({
+  estado: {
+    type: String,
+    required: true,
+  },
+})
+
+const badgeClass = computed(() => ({
+  [ESTADOS_RESERVA.PENDIENTE]:  'text-warning border-warning',
+  [ESTADOS_RESERVA.CONFIRMADA]: 'text-success border-success',
+  [ESTADOS_RESERVA.CANCELADA]:  'text-red-700 border-red-700',
+}[props.estado]))
+</script>
