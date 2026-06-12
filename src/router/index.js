731ea261a49e8b121e4206c9a9b1bde8449c709b
Ivan riboldi <38292854@comunidadort.edu.ar>
2026-06-12 15:51:35 -0300

    se agregan routas mas la autenticacion

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,50 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "../stores/auth.js";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: ()=> import("../views/HomeView.vue")
+    path: "/login",
+    name: "login",
+    component: () => import("../views/auth/LoginView.vue"),
   },
   {
-    path: '/mentor/:id',
-    name: 'mentorDetail',
-    component: ()=> import("../views/MentorDetailView.vue")
+    path: "/",
+    name: "home",
+    component: () => import("../views/student/HomeView.vue"),
+    meta: { requiresAuth: true, rol: "student" },
   },
   {
-    path: '/mis-mentorias',
-    name: 'misMentorias',
-    component: ()=> import("../views/MisMentoriasView.vue")
-  }
-]
+    path: "/mentor/:id",
+    name: "mentorDetail",
+    component: () => import("../views/student/MentorDetailView.vue"),
+    meta: { requiresAuth: true, rol: "student" },
+  },
+  {
+    path: "/mis-mentorias",
+    name: "misMentorias",
+    component: () => import("../views/student/MisMentoriasView.vue"),
+    meta: { requiresAuth: true, rol: "student" },
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/",
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to) => {
+  const authStore = useAuthStore();
+  if (to.meta.requiresAuth && !authStore.isLoggedIn) {
+    return { name: "login" };
+  }
+  if (to.name === "login" && authStore.isLoggedIn) {
+    //TODO:modificar la ruta del mentor por el nombre correcto
+    return authStore.isMentor ? { name: "mentorDashboard" } : { name: "home" };
+  }
+});
+
+export default router;
