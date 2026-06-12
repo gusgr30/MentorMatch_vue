a0ae7bba2a877008aa9846903fc6af3a4bda2df0
Ivan riboldi <38292854@comunidadort.edu.ar>
2026-06-12 15:11:58 -0300

    se crea estado global con pinia para useAuthStore

---
+++ src/stores/auth.js
@@ -0,0 +1,51 @@
+import { defineStore } from 'pinia'
+import api from '../api/axios.js'
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null,
+    loading: false,
+    error: null,
+  }),
+
+  getters: {
+    isLoggedIn: (state) => !!state.user,
+    isStudent: (state) => state.user?.rol === 'student',
+    isMentor: (state) => state.user?.rol === 'mentor',
+  },
+
+  actions: {
+    async login(email, password) {
+      this.loading = true
+      this.error = null
+      try {
+        const { data } = await api.post('/auth/login', { email, password })
+        this.user = data.user
+        localStorage.setItem('token', data.token)
+        api.defaults.headers['Authorization'] = `Bearer ${data.token}`
+      } catch (err) {
+        this.error = err.response?.data?.error ?? err.message
+        throw err
+      } finally {
+        this.loading = false
+      }
+    },
+
+//TODO:agregar register
+
+    logout() {
+      this.user = null
+      localStorage.removeItem('token')
+      delete api.defaults.headers['Authorization']
+    },
+
+    async init() {
+      const token = localStorage.getItem('token')
+      if (!token) return
+
+      const { data } = await api.get('/auth/me')
+      this.user = data.user
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
+    }
+  }
+})
