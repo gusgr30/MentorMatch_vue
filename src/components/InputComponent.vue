0e0a38a1d4f5d9742458d7b83e2dc4ef1e2aa819
GusGR23 <gustavoraguso@hotmail.com>
2026-06-16 18:18:44 -0300

    se implemento el cambio de visibilidad del password

--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -3,18 +3,31 @@
         <label :class="VARIANT_CLASS.LABEL">
             {{ props.label.toUpperCase() }}
         </label>
-        <input 
-            :type="props.type" 
-            v-model="modelValue" 
-            :placeholder="props.placeholder"      
-            :disabled="props.disabled"
-            :class="variantClass">
+        <div class="relative">
+            <input 
+                :type="inputType" 
+                v-model="modelValue" 
+                :placeholder="props.placeholder"      
+                :disabled="props.disabled"
+                :class="variantClass">
+
+            <button 
+                v-if="props.type === 'password'" 
+                type="button"
+                @click="togglePasswordVisibility"
+                class="absolute right-4 top-1/2 -translate-y-1/2 text-slate-400 hover:text-indigo-600 transition"
+            >
+                <EyeOff v-if="isPasswordVisible" :size="20" />
+                <Eye v-else :size="20" />
+            </button>
+        </div>
     </div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { VARIANT_CLASS } from '../constants/styles.js'
+import { Eye, EyeOff, ToggleLeft } from '@lucide/vue'
 
 const props = defineProps({
     label: {
@@ -39,9 +52,23 @@ const modelValue = defineModel()
 
 
 const variantClass = computed(() => {
-  if (props.disabled) return VARIANT_CLASS.DISABLED //modificar estilo para el boton disabled
+  if (props.disabled) return VARIANT_CLASS.DISABLED
 
   return VARIANT_CLASS.DEFAULT
 })
+
+const isPasswordVisible = ref(false)
+
+const inputType = computed(() => {
+    if(props.type === 'password' && isPasswordVisible.value) return 'text'
+    
+    return props.type
+})
+
+const togglePasswordVisibility = () => {
+    isPasswordVisible.value = !isPasswordVisible.value
+}
+
+
 
 </script>
